d520503221697cb288646467800534771dedd36f
Caleb Mcolin <Caleb.mcolin@gmail.com>
2022-07-12 15:32:45 -0700

    removed boilerplate

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 import './style.css'
+import * as THREE from 'three';
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+
